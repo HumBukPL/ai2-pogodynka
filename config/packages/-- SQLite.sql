06384f6657c1ac65e948277256481c9fea9a4a9d
HumBukPL <k.kolodziejczykk01@gmail.com>
2023-11-15 22:08:11 +0100

    lab-e

---
+++ config/packages/-- SQLite.sql
@@ -0,0 +1,5 @@
+-- SQLite
+INSERT INTO user ( username, roles, password)
+VALUES ('user', '["ROLE_USER"]', '$2y$13$z4UwTBPc5EYGeaLMS1RRdObiI6IhB.j1XJg1Dz1fD1quoy9AM0Sfq');
+-- clear user table
+-- DELETE FROM user;
